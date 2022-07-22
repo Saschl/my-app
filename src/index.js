af0fdba9cb884e405c0af498983e47df7ad454ad
Sascha Rudolf <sascha.rudolf@sap.com>
2022-07-22 13:43:40 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
+import { ThemeProvider, SelectDialog, StandardListItem } from '@ui5/webcomponents-react';
 import '@ui5/webcomponents-react/dist/Assets.js';
 import '@ui5/webcomponents/dist/features/InputElementsFormSupport.js';
-import { ThemeProvider } from '@ui5/webcomponents-react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './util/i18n';
-import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <ThemeProvider>
-    <App />
+    <>
+      <SelectDialog growing="Scroll" headerText="Select Product" onLoadMore={() => console.log('Growing!')} open={true}>
+        {new Array(100).fill('a', 0, 100).map((s, index) => (
+          <StandardListItem key={index}>{index}</StandardListItem>
+        ))}
+      </SelectDialog>
+    </>
   </ThemeProvider>
 );
